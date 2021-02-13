914a1b0caafcbcc4deb7a3c1b26074f6046e27f3
Fatih <kresnofatihimani.private@gmail.com>
2021-02-13 12:17:30 +0700

    login/signup-to-firestore functions added :rocket:

--- src/App.js
+++ src/App.js
@@ -5,18 +5,63 @@ import fire from './Fire'
 import firebase from 'firebase'
 
 function App() {
-  // state
-  const [user, setUser] = useState('')
+  // states
+  const [user, setUser] = useState('') // google user auth data
+  const [profile, setProfile] = useState('') // firestore user auth data
 
-  // login
+  // function: if first time logged, create a user doc in firestore
+  const initializeUserDataInFireStore = async (user) => {
+    if(user.metadata.creationTime===user.metadata.lastSignInTime){
+      await fire
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .set({
+          uid: user.uid,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          email: user.email,
+          friends: [],
+          status: 'online',
+          chatrooms: []
+        })
+    }
+  }
+  
+  // function: update status to online/offline if not first time logged in
+  const updateUserStatusInFirestore = async (user, authAction) => {
+    if(user.metadata.creationTime!==user.metadata.lastSignInTime && authAction==='login'){
+      await fire
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .update({
+          status: 'online'
+        })
+    }
+    else if(user.metadata.creationTime!==user.metadata.lastSignInTime && authAction==='logout'){
+      await fire
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .update({
+          status: 'offline'
+        })
+    }
+  }
+
+
+  // login method
   const login = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
     fire
       .auth()
       .signInWithPopup(provider)
       .then((result)=>{
-          const token = result.credential.accessToken;
           const user = result.user;
+          // if first time logged, create a user doc in firestore
+          initializeUserDataInFireStore(user);
+          updateUserStatusInFirestore(user, 'login');
       })
       .catch((error)=>{
           console.log(error);
@@ -36,14 +81,28 @@ function App() {
       })
   }
 
+  // profileListener
+  const profileListener = () => {
+    fire
+      .firestore()
+      .collection('users')
+      .doc(user.uid)
+      .onSnapshot(docSnapshot=>{
+        setProfile(docSnapshot.data());
+      })
+  }
+
   // logout
   const logout = () => {
-    fire.auth().signOut();
+    updateUserStatusInFirestore(user, 'logout').then(()=>{
+      fire.auth().signOut();
+    })
   };
 
   // authlistener on refresh
   useEffect(()=>{
     authListener();
+    profileListener();
   }, [])
 
   return (
